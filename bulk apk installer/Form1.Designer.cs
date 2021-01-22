60a46235a1155d658f979f5a8c6b398819357133
00sanoj00 <00sanoj00@gmail.com>
2021-01-22 21:16:11 +0530

    first commit

---
+++ bulk apk installer/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+using System.Windows.Forms;
+namespace bulk_apk_installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialMultiLineTextBox1 = new MaterialSkin.Controls.MaterialMultiLineTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialMultiLineTextBox1
+            // 
+            this.materialMultiLineTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.materialMultiLineTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialMultiLineTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialMultiLineTextBox1.Depth = 0;
+            this.materialMultiLineTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialMultiLineTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialMultiLineTextBox1.Hint = "";
+            this.materialMultiLineTextBox1.Location = new System.Drawing.Point(11, 290);
+            this.materialMultiLineTextBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialMultiLineTextBox1.Name = "materialMultiLineTextBox1";
+            this.materialMultiLineTextBox1.Size = new System.Drawing.Size(355, 387);
+            this.materialMultiLineTextBox1.TabIndex = 0;
+            this.materialMultiLineTextBox1.Text = "";
+            this.materialMultiLineTextBox1.TextChanged += new System.EventHandler(this.materialMultiLineTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(11, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(355, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Install APK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 93);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(358, 152);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(350, 126);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Bulk APK";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 61);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select APK\'S Folder";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(6, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = ":::";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(53, 18);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 33);
+            this.textBox1.TabIndex = 4;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(350, 126);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Single APK";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Location = new System.Drawing.Point(41, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "No Device has connected to this PC";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(11, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 24);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(379, 689);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.materialMultiLineTextBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1500, 1900);
+            this.MinimumSize = new System.Drawing.Size(261, 65);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bulk APK Installer";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialMultiLineTextBox materialMultiLineTextBox1;
+        private Button button1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button3;
+    }
+}
+
---
+++ bulk apk installer/Form1.cs
@@ -0,0 +1,266 @@
+﻿
+
+using MaterialSkin.Controls;
+using SharpAdbClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Windows.Forms;
+using WK.Libraries.BetterFolderBrowserNS;
+
+namespace bulk_apk_installer
+{
+    public partial class Form1 : MaterialForm
+    {
+
+        StreamWriter stdin = null;
+        String path = null;
+        String formname = "Bulk APK Installer";
+        String pathwithot = "";
+        int install_count = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = formname;
+
+        }
+
+        public void installadb()
+        {
+            AdbServer server = new AdbServer();
+            var result = server.StartServer("adb.exe", restartServerIfNewer: false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            installadb();
+            loadtext();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          //                 
+            
+        }
+
+
+        private void StartCmdProcess()
+        {
+            ProcessStartInfo pStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "start /WAIT",
+                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+
+            };
+
+            Process cmdProcess = new Process
+            {
+
+                StartInfo = pStartInfo,
+                EnableRaisingEvents = true,
+            };
+
+            cmdProcess.Start();
+            materialMultiLineTextBox1.Text = string.Empty;
+            cmdProcess.BeginErrorReadLine();
+            cmdProcess.BeginOutputReadLine();
+            stdin = cmdProcess.StandardInput;
+
+            cmdProcess.OutputDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new MethodInvoker(() =>
+                    {
+                        materialMultiLineTextBox1.AppendText(evt.Data + Environment.NewLine);
+                        materialMultiLineTextBox1.ScrollToCaret();
+                    }));
+                }
+            };
+
+            cmdProcess.ErrorDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new Action(() =>
+                    {
+                        //rtbStdErr.AppendText(evt.Data + Environment.NewLine);
+                        //rtbStdErr.ScrollToCaret();
+                    }));
+                }
+            };
+
+            cmdProcess.Exited += (s, evt) =>
+            {
+                // cmdProcess?.Dispose();
+            };
+        }
+
+        public void displaydevice()
+        {
+            var devices = AdbClient.Instance.GetDevices();
+            foreach (var device in devices)
+            {
+                if (device.Model == "")
+                {
+                    label1.Text = "No Device has connected to this PC";
+                }
+                else
+                {
+                    label1.Text = $"The device " + device.Model + " has connected to this PC";
+                }   
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string a = label1.Text;
+            string b = textBox1.Text;
+
+            if (a.Equals("No Device has connected to this PC"))
+            {
+                MessageBox.Show("No Device has connected to this PC");
+            }else
+            {
+                if (b.Equals(string.Empty))
+                {
+                    MessageBox.Show("Please select the folder containing the apk file");
+                }
+                else
+                {
+                    string installcom = "for %e in (" + path + "*.apk) do adb install -r \"%e\"";
+                    StartCmdProcess();
+                    
+                    try
+                    {
+                        stdin.Write("\u0040echo off" + Environment.NewLine);
+                        stdin.Write(installcom + Environment.NewLine);
+                    }
+                    catch
+                    {
+
+                    }
+                    finally
+                    {
+                        
+                    }
+                    
+                    
+                    
+                }
+                
+            }
+
+            
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            install_count = 0;
+            BetterFolderBrowser bfb = new BetterFolderBrowser();
+            bfb.Title = "Choose any folder as the root folder...";
+
+            if (bfb.ShowDialog() == DialogResult.OK)
+                textBox1.Text = bfb.SelectedPath;
+
+            path = bfb.SelectedPath+ "\u005C";
+            pathwithot = bfb.SelectedPath;
+
+
+
+
+
+        }
+
+        private void materialMultiLineTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                replace();
+
+                int maxLines = materialMultiLineTextBox1.Lines.Length;
+                string lastLine = materialMultiLineTextBox1.Lines[maxLines - 2];
+                string lastWord = lastLine.Split(' ').Last();
+
+                if (lastLine == "Success")
+                {
+                    install_count++;
+                    progresstext();
+                }
+            }
+            catch
+            {
+
+            }
+            
+        
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            
+
+            displaydevice();
+            string a = label1.Text;
+
+            if (a == "No Device has connected to this PC")
+            {
+            }
+            else
+            {
+                label1.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+        void progresstext()
+        {
+            materialMultiLineTextBox1.AppendText("-------------------------------" + Environment.NewLine+ "--Build APK Installer--" + Environment.NewLine+ "--   Installed  "+install_count+" apk   --" + Environment.NewLine + "--------------------------------" + Environment.NewLine);
+        }
+        void loadtext()
+        {
+            materialMultiLineTextBox1.AppendText("..... (¯`v´¯)♥" + Environment.NewLine + ".......•.¸.•´" + Environment.NewLine + "....¸.•´" + Environment.NewLine + "... (" + Environment.NewLine + "☻/" + Environment.NewLine + "/▌♥♥" + Environment.NewLine + "/ \u005C ♥♥" + Environment.NewLine );
+            materialMultiLineTextBox1.AppendText("-------------------------------------------------------------" + Environment.NewLine);
+            materialMultiLineTextBox1.AppendText("Bulk APK Installer" + Environment.NewLine);
+            materialMultiLineTextBox1.AppendText("By Sanoj Prabatb Jayathilaka" + Environment.NewLine);
+            materialMultiLineTextBox1.AppendText("Github - github.com/00sanoj00" + Environment.NewLine);
+            materialMultiLineTextBox1.AppendText("Whatsapp - +94716474696");
+            materialMultiLineTextBox1.AppendText("Facebook - fb.com/sanoj.jayathilaka1" + Environment.NewLine);
+            materialMultiLineTextBox1.AppendText("-------------------------------------------------------------" + Environment.NewLine);
+
+        }
+        void replace()
+        {
+            String Directory = System.Windows.Forms.Application.StartupPath;
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace(Directory, "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace(">", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("for %e in", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("%", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("(", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace(path, "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("*.apk)", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("do", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("adb install", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("@echo", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("off", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("-r", "");
+            materialMultiLineTextBox1.Text = materialMultiLineTextBox1.Text.Replace("\"%e\"", "");
+        }
+
+    }
+}
